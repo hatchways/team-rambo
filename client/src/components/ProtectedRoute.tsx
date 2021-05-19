f40e203e372007aaa2fe8ec5879a81b7fc9ecdcf
street-jackal <jon.mcintire.myers@gmail.com>
2021-05-19 16:24:40 -0700

    refactored to match style of other components

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -2,10 +2,12 @@ import { CircularProgress } from '@material-ui/core';
 import { Route, Redirect, RouteProps } from 'react-router-dom';
 import { useAuth } from '../context/useAuthContext';
 
-export default function ProtectedRoute({ ...routeProps }: RouteProps): JSX.Element {
+const ProtectedRoute = (routeProps: RouteProps): JSX.Element => {
   const { loggedInUser } = useAuth();
 
   // Show circular progress bar while waiting on response from useAuth
   if (loggedInUser === undefined) return <CircularProgress />;
   else return loggedInUser ? <Route {...routeProps} /> : <Redirect to={{ pathname: '/login' }} />;
-}
+};
+
+export default ProtectedRoute;
