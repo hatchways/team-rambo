fcc9c692cd28ad5230907ca4a81e2241ddd9167e
Gabriel <gabrieldallavecchia@live.com>
2021-05-18 21:29:19 -0600

    some changes

--- server/models/Board.js
+++ server/models/Board.js
@@ -2,10 +2,6 @@ const mongoose = require("mongoose");
 const columnSchema = require("./Column");
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
   email: {
     type: String,
     required: true,
--- server/models/User.js
+++ server/models/User.js
@@ -2,11 +2,6 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
   email: {
     type: String,
     required: true,
