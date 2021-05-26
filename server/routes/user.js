adcc03a4a2fc8038a88885228da27887012b56f5
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-05-26 14:46:43 -0700

    Add new boards routes and comments on users

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const protect = require("../middleware/auth");
-const {
-  searchUsers,
-  createBoard,
-  getUserBoards,
-} = require("../controllers/user");
+const { searchUsers, getUserBoards } = require("../controllers/user");
 
+// A `/` route typically is an index
+// If searching users, should consider either a :id, or string query=...
 router.route("/").get(protect, searchUsers);
-
-router.route("/board").post(protect, createBoard);
 router.route("/board").get(protect, getUserBoards);
 
 module.exports = router;
