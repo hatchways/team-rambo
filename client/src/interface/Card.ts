d9717e6f5ca94326a4859107e9e541ad245c0ad0
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-05-27 18:00:59 -0700

    Remove unnecessary interfaces to retain consistency across the app

--- client/src/interface/Card.ts
+++
@@ -1,6 +0,0 @@
-export interface Card {
-  _id: string;
-  name: string;
-  description: string;
-  createdAt: string;
-}
--- client/src/interface/Column.ts
+++
@@ -1,8 +0,0 @@
-import { Card } from './Card';
-
-export interface Column {
-  _id: string;
-  name: string;
-  cards: Array<Card>;
-  createdAt: string;
-}
