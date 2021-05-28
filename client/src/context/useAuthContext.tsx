d3d8de7f6304f1c53d35d78e92fc35e6a28bec04
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-05-28 14:45:14 -0700

    Fix code structure to use returns and either await or .then

--- client/src/context/useAuthContext.tsx
+++ client/src/context/useAuthContext.tsx
@@ -2,8 +2,7 @@ import { useState, useContext, createContext, FunctionComponent, useEffect, useC
 import { useHistory } from 'react-router-dom';
 import { AuthApiData, AuthApiDataSuccess } from '../interface/AuthApiData';
 import { User } from '../interface/User';
-import loginWithCookies from '../helpers/APICalls/loginWithCookies';
-import logoutAPI from '../helpers/APICalls/logout';
+import { loginWithCookies, logout as logoutApi } from '../helpers/';
 
 interface IAuthContext {
   loggedInUser: User | null | undefined;
@@ -26,16 +25,20 @@ export const AuthProvider: FunctionComponent = ({ children }): JSX.Element => {
     (data: AuthApiDataSuccess) => {
       setLoggedInUser(data.user);
       history.push('/dashboard');
+
+      return;
     },
     [history],
   );
 
   const logout = useCallback(async () => {
     // needed to remove token cookie
-    await logoutAPI()
+    logoutApi()
       .then(() => {
         history.push('/login');
         setLoggedInUser(null);
+
+        return;
       })
       .catch((error) => console.error(error));
   }, [history]);
@@ -43,14 +46,17 @@ export const AuthProvider: FunctionComponent = ({ children }): JSX.Element => {
   // use our cookies to check if we can login straight away
   useEffect(() => {
     const checkLoginWithCookies = async () => {
-      await loginWithCookies().then((data: AuthApiData) => {
+      loginWithCookies().then((data: AuthApiData) => {
         if (data.success) {
           updateLoginContext(data.success);
           history.push('/dashboard');
-        } else {
-          // don't need to provide error feedback as this just means user doesn't have saved cookies or the cookies have not been authenticated on the backend
-          setLoggedInUser(null);
+
+          return;
         }
+
+        setLoggedInUser(null);
+
+        return;
       });
     };
     checkLoginWithCookies();
