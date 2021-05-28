3cb7db07169c71937f6371e5b08d3c8fcf59fa8b
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-05-28 14:58:19 -0700

    Create index file to export context objects

---
+++ client/src/context/index.ts
@@ -0,0 +1,5 @@
+export { useAuth, AuthContext, AuthProvider } from './useAuthContext';
+export { useKanban, KanbanContext, KanbanProvider } from './useKanbanContext';
+export { useSocket, SocketContext, SocketProvider } from './useSocketContext';
+export { useSnackBar, SnackBarContext, SnackBarProvider } from './useSnackbarContext';
+export { useDialog, DialogContext, DialogProvider } from './useDialogContext';
