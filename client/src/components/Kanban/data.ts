6faf96a0dd9f57eda4e1bf0d0c077abf34b6b02d
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-05-27 20:28:26 -0700

    Create comment to display redundancy

--- client/src/components/Kanban/data.ts
+++ client/src/components/Kanban/data.ts
@@ -1,12 +1,14 @@
 import { IColumn } from '../../context/types/kanban';
 
+// Shouldn't use this object to create data
+
 export const columnData: IColumn[] = [
   {
     _id: 'col-1',
     name: 'Philosophy',
     cards: [
       {
-        id: 'car-1',
+        _id: 'car-1',
         columnId: 'col-1',
         name: 'Essay on the environment',
         tag: 'green',
@@ -18,7 +20,7 @@ export const columnData: IColumn[] = [
     name: 'Math',
     cards: [
       {
-        id: 'car-2',
+        _id: 'car-2',
         columnId: 'col-2',
         name: 'Midterm exam',
         dueDate: new Date(),
@@ -31,7 +33,7 @@ export const columnData: IColumn[] = [
     name: 'In Progress',
     cards: [
       {
-        id: 'car-3',
+        _id: 'car-3',
         columnId: 'car-3',
         name: 'Homework',
         tag: 'red',
@@ -44,20 +46,20 @@ export const columnData: IColumn[] = [
     name: 'Done',
     cards: [
       {
-        id: 'car-4',
+        _id: 'car-4',
         columnId: 'col-4',
         name: 'Workshop',
         tag: 'yellow',
         // other data (assignees, due dates, etc.)
       },
       {
-        id: 'car-5',
+        _id: 'car-5',
         columnId: 'col-4',
         name: 'Practice exam',
         tag: 'red',
       },
       {
-        id: 'car-6',
+        _id: 'car-6',
         columnId: 'col-4',
         name: 'Research',
         tag: 'green',
