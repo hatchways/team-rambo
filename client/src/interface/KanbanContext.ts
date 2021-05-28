256d613a6b336c51e5e4e4dafa56b309aba8f7ad
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-05-28 15:19:41 -0700

    Fix context interfaces for app usage

--- client/src/interface/KanbanContext.ts
+++ client/src/interface/KanbanContext.ts
@@ -1,13 +1,15 @@
 import { DropResult } from 'react-beautiful-dnd';
+import { IBoard } from './Board';
 import { ICard } from './Card';
 import { IColumn } from './Column';
 
 export interface IKanbanContext {
-  columns: IColumn[];
+  activeBoard: IBoard;
   focusedCard: ICard | null;
-  handleDragEnd: (result: DropResult) => void;
   addCard: (card: ICard) => boolean;
   setOpenCard: (card: ICard) => void;
   resetOpenCard: () => void;
   getColumnById: (columnId: string) => IColumn | null;
+  handleDragEnd: (result: DropResult) => void;
+  setActiveBoard: (board: IBoard) => void;
 }
