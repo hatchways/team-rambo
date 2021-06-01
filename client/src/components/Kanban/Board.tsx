f5789d1782eb11d3309d9c1c56c35d8015e04bf3
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-06-01 16:17:54 -0700

    Refactor for cleaner code and use of createNewBoard from KanbanContext

--- client/src/components/Kanban/Board.tsx
+++ client/src/components/Kanban/Board.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import { Grid } from '@material-ui/core';
 import { DragDropContext, Droppable, DroppableProvided } from 'react-beautiful-dnd';
-import { IBoard } from '../../interface/';
-import { useKanban } from '../../context/';
 import { FocusCard } from './FocusCard/FocusCard';
 import Column from './Column/Column';
+import { IBoard } from '../../interface/';
+import { useKanban } from '../../context/';
 
 const Board = ({ activeBoard }: { activeBoard: IBoard }): JSX.Element => {
   const { handleDragEnd } = useKanban();
@@ -12,26 +11,24 @@ const Board = ({ activeBoard }: { activeBoard: IBoard }): JSX.Element => {
   return (
     <DragDropContext onDragEnd={handleDragEnd}>
       <Droppable droppableId="board" type="column" direction="horizontal">
-        {(provided: DroppableProvided) => {
-          return (
-            <Grid ref={provided.innerRef} container spacing={2} {...provided.droppableProps}>
-              {activeBoard.columns.map((column, index) => (
-                <>
-                  <FocusCard />
-                  <Column
-                    key={column._id}
-                    index={index}
-                    _id={column._id}
-                    name={column.name}
-                    cards={column.cards}
-                    createdAt={column.createdAt}
-                  />
-                </>
-              ))}
-              {provided.placeholder}
-            </Grid>
-          );
-        }}
+        {(provided: DroppableProvided) => (
+          <Grid ref={provided.innerRef} container spacing={2} {...provided.droppableProps}>
+            {activeBoard.columns.map((column, index) => (
+              <>
+                <FocusCard />
+                <Column
+                  key={column._id}
+                  index={index}
+                  _id={column._id}
+                  name={column.name}
+                  cards={column.cards}
+                  createdAt={column.createdAt}
+                />
+              </>
+            ))}
+            {provided.placeholder}
+          </Grid>
+        )}
       </Droppable>
     </DragDropContext>
   );
