1c2eca0d690456b29caec94b8d4634e8985264c9
Ethan Moffat <ethanmoffat@hey.com>
2021-06-02 16:19:31 -0700

    fix(dialog-renders) fixed bug where dialogs were rendering multiple times

--- client/src/components/Kanban/Board.tsx
+++ client/src/components/Kanban/Board.tsx
@@ -5,32 +5,34 @@ import { FocusCard } from './FocusCard/FocusCard';
 import Column from './Column/Column';
 
 const Board = (): JSX.Element => {
-  const { columns, handleDragEnd } = useKanban();
+  const { focusedCard, columns, handleDragEnd } = useKanban();
   return (
-    <DragDropContext onDragEnd={handleDragEnd}>
-      <Droppable droppableId="board" type="column" direction="horizontal">
-        {(provided: DroppableProvided) => {
-          return (
-            <Grid ref={provided.innerRef} container spacing={2} {...provided.droppableProps}>
-              {columns.map((column, index) => (
-                <>
-                  <FocusCard />
-                  <Column
-                    key={column.id}
-                    index={index}
-                    id={column.id}
-                    name={column.name}
-                    cards={column.cards}
-                    createdAt={column.createdAt}
-                  />
-                </>
-              ))}
-              {provided.placeholder}
-            </Grid>
-          );
-        }}
-      </Droppable>
-    </DragDropContext>
+    <>
+      {focusedCard && <FocusCard />}
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <Droppable droppableId="board" type="column" direction="horizontal">
+          {(provided: DroppableProvided) => {
+            return (
+              <Grid ref={provided.innerRef} container spacing={2} {...provided.droppableProps}>
+                {columns.map((column, index) => (
+                  <>
+                    <Column
+                      key={column.id}
+                      index={index}
+                      id={column.id}
+                      name={column.name}
+                      cards={column.cards}
+                      createdAt={column.createdAt}
+                    />
+                  </>
+                ))}
+                {provided.placeholder}
+              </Grid>
+            );
+          }}
+        </Droppable>
+      </DragDropContext>
+    </>
   );
 };
 
