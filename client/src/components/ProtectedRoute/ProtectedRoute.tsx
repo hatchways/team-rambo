1bc2c4d67fbe23ceb89357eb7de3f5536ae8df86
Gabriel <gabrieldallavecchia@live.com>
2021-06-08 16:49:51 -0600

    addded the LoadingBoard to the protected route component

--- client/src/components/ProtectedRoute/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,26 +1,12 @@
-import { CircularProgress, Grid, Typography } from '@material-ui/core';
 import { Route, Redirect, RouteProps } from 'react-router-dom';
 import { useAuth } from '../../context/useAuthContext';
-import useStyles from './protectedRouteStyles';
+import LoadingBoard from '../../components/LoadingBoard/LoadingBoard';
 
 const ProtectedRoute = (routeProps: RouteProps): JSX.Element => {
   const { loggedInUser } = useAuth();
-  const classes = useStyles();
 
   // Show circular progress bar while waiting on response from useAuth
-  if (loggedInUser === undefined)
-    return (
-      <Grid spacing={4} container justify="center" className={classes.loadingScreen}>
-        <Grid item>
-          <CircularProgress size={150} />
-        </Grid>
-        <Grid item>
-          <Typography variant="h4" className={classes.loadingScreenText}>
-            Kanban
-          </Typography>
-        </Grid>
-      </Grid>
-    );
+  if (loggedInUser === undefined) return <LoadingBoard />;
   else return loggedInUser ? <Route {...routeProps} /> : <Redirect to={{ pathname: '/login' }} />;
 };
 
