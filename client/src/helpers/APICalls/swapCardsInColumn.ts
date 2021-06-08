318f1b68584c50065bb29513a0b6892d406357ff
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-06-08 14:28:43 -0700

    Add new helper functions for specific functionality from the API

---
+++ client/src/helpers/APICalls/swapCardsInColumn.ts
@@ -0,0 +1,23 @@
+import { DraggableLocation } from 'react-beautiful-dnd';
+import { IBoard, IFetchOptions } from '../../interface';
+
+const swapCardsInColumn = async (
+  cardId: string,
+  source: DraggableLocation,
+  destination: DraggableLocation,
+): Promise<IBoard> => {
+  const fetchOptions: IFetchOptions = {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'include',
+    body: JSON.stringify({ cardId, source, destination }),
+  };
+
+  return await fetch(`columns/${source.droppableId}/swapCardsInColumn`, fetchOptions)
+    .then((res) => res.json())
+    .catch((err) => ({
+      error: { error: err, message: 'Unable to connect to server. Please try again' },
+    }));
+};
+
+export default swapCardsInColumn;
